e659a260fb225b0a8b2ce8c40480163a84075586
Ravn <gabriell.ribeiro@yahoo.com>
2023-09-02 16:52:48 -0300

    feat: updating projects details

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -25,10 +25,10 @@ function Projects() {
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Full-stack Social Network"
+              description="Full-stack application (Front-end, Back-end, and Mobile) of a social network called PARROT 🦜. There are also implementations of Swagger, containerization, buckets and message brokers.The Backend part has two versions: one in Node.js and another in Java"
+              ghLink="https://github.com/bc-fullstack-02/Gabriel-Ribeiro"
+              demoLink="https://github.com/Gahbr/Rede-Social-Java-Spring"
             />
           </Col>
 
@@ -36,10 +36,10 @@ function Projects() {
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="URL-Shortener"
+              description= "URL shortener microservice in NodeJS - ExpressJS - MongoDB"
+              ghLink="https://github.com/Gahbr/URL-Shortener"
+              demoLink="https://shorten.up.railway.app/"
             />
           </Col>
 
@@ -47,10 +47,10 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Sales Report"
+              description="Application that makes a report of the sales made by the sellers, filtered according to the date chosen by the user and also with an option to send an SMS with the data of the chosen seller."
+              ghLink="https://github.com/Gahbr/dsmeta"
+              demoLink="https://dsmeta-page.netlify.app/"              
             />
           </Col>
 
@@ -58,10 +58,9 @@ function Projects() {
             <ProjectCard
               imgPath={leaf}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Covid Tracker Application"
+              description="Java Spring Boot application that track reported data of confirmed Coronavirus infections COVID-19 (2019-nCoV) around the world."
+              ghLink="https://github.com/Gahbr/covid-tracker-app"
             />
           </Col>
 
@@ -69,22 +68,19 @@ function Projects() {
             <ProjectCard
               imgPath={suicide}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              title="To-do App"
+              description="CRUD application for project managing and the tasks envolved in those projects. This application was made on Java Swing."
+              ghLink="https://github.com/Gahbr/Todo-App-Java"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              isBlog={true}
+              title="Other Projects"
+              description="Click on the link above to see my other projects that I'm constantly building and updating!"
+              ghLink="https://github.com/Gahbr?tab=repositories"
             />
           </Col>
         </Row>
